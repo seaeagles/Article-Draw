6f6a61b15e2db96c27e1cb26beb50e187a61e88f
David M <dminerur@outlook.com>
2023-01-31 14:41:17 -0500

    variables and async function

---
+++ main.js
@@ -0,0 +1,29 @@
+// https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty
+// https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
+// beststories
+// newstories
+
+
+const topp = document.querySelector('#top')
+const best = document.querySelector('#best')
+const fresh = document.querySelector('#new')
+const stories = document.querySelector('#stories')
+
+console.log('top', topp)
+topp.addEventListener('click', e => console.log('top'))
+best.addEventListener('click', e => fetchStories('beststories'))
+fresh.addEventListener('click', e => fetchStories('newstories'))
+
+async function fetchStories (path) {
+    const response = await fetch(
+        `https://hacker-news.firebaseio.com/v0/${path}stories.json?print=pretty`
+        )
+    const data = await response.json()
+    const promisesArray = await data.slice(0, 20).map(id => fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+        ).then(resp => resp.json()))
+
+    const storiesArray = await Promise.all(promisesArray)
+    console.log('stories', stories)
+    return storiesArray
+}
